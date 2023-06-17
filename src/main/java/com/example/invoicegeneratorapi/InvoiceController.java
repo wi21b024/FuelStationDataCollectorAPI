0e5d2ba020392d764aa56ac09952d5d0488e3249
wi21b024 <wi21b024@technikum-wien.at>
2023-06-17 16:40:13 +0200

    Denise

--- src/main/java/com/example/invoicegeneratorapi/InvoiceController.java
+++ src/main/java/com/example/invoicegeneratorapi/InvoiceController.java
@@ -1,2 +1,45 @@
-package com.example.invoicegeneratorapi;public class InvoiceController {
+package com.example.invoicegeneratorapi;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.rabbitmq.client.ConnectionFactory;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/invoices")
+@Slf4j
+public class InvoiceController {
+    public static final String CUSTOMER_ID_CHANNEL = "CustomerIDChannel";
+    private static Logger logger = LoggerFactory.getLogger(InvoiceController.class);
+
+    @PostMapping("/{customerID}")
+    public ResponseEntity<Void> generateInvoice(@PathVariable String customerID) {
+        logger.info(customerID+" received!");
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(30003);
+
+        try {
+            try (
+                    com.rabbitmq.client.Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel()
+            ) {
+                channel.queueDeclare(CUSTOMER_ID_CHANNEL, false, false, false, null);
+                channel.basicPublish("", CUSTOMER_ID_CHANNEL, null, customerID.getBytes(StandardCharsets.UTF_8));
+                logger.info("Published to queue " + CUSTOMER_ID_CHANNEL+ " message "+customerID);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    //@GetMapping("/{customerID}")
 }
+
