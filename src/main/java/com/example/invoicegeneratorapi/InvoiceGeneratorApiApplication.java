259070972a820497c805ecd081c4e15ecaafeac6
wi21b024 <wi21b024@technikum-wien.at>
2023-06-17 16:28:53 +0200

    Initial commit

---
+++ src/main/java/com/example/invoicegeneratorapi/InvoiceGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoicegeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/invoicegeneratorapi/InvoiceGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicegeneratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
